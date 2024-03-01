21376646b7dbec589a28931ae73c8a2dfc105e5e
Roshan <roshan77karki77@gmail.com>
2024-03-01 11:38:09 +0545

    responsive2 completed

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center text-xs">
-      <div className="bg-green-0 intro w-full h-full">
-        <div className="introtext">
-          <div className="introduction font-bold mb-3">Introduction res2</div>
+    <main className="flex min-h-screen flex-col items-center text-xs ">
+      <div className="intro h-full bg-[url('/introBg.png')] bg-cover bg-[#e8ffdb] lg:bg-cover">
+        <div className="introtext w-64 relative left-12 lg: top-10 left-40">
+          <div className="introduction font-bold mb-3">Introduction</div>
           <div className="introlinks list-none flex">
             <li>
               <a href="#" className="mr-2 text-green-800">Home</a>
@@ -21,9 +21,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="max-w-[1210px] aboutcontent pt-5">
-        <div className="flex pb-20">
-          <div className="flex flex-col text-justify mr-10 mt-10 basis-3/5 max-w-[678px] mx-auto">
+      <div className=" aboutcontent pt-5 flex flex-col max-w-[1210px] px-11 lg:pl-0">
+        <div className="flex flex-col pb-20 lg:flex-row">
+          <div className="flex flex-col text-justify  mt-10 basis-3/5 max-w-[678px] mx-auto lg:mr-10">
             <div className="objective mb-6">
               With the main objective of providing institutional credit for
               enhancing the production and productivity of the agricultural
@@ -54,15 +54,15 @@ export default function Home() {
             <img src="/officebuilding.png" alt="building" className="officebuilding" />
           </div>
         </div>
-        <div className="goals flex">
-          <div className="goal mr-8 basis-1/3">
+        <div className="flex flex-col lg:flex-row">
+          <div className="goal basis-1/3 mb-4 lg:mr-8 ">
             <img src="/vision.png" alt="vision" />
             <div className="goaltitle">Vision</div>
             <div className="goaltext">
               To be a Mass-based Complete Bank serving from Urban to Rural.
             </div>
           </div>
-          <div className="goal mr-8 basis-1/3">
+          <div className="goal basis-1/3 mb-4 lg:mr-8 ">
             <img src="/mission.png" alt="mission" />
             <div className="goaltitle">Mission</div>
             <div className="goaltext">
@@ -70,7 +70,7 @@ export default function Home() {
               extensive network.
             </div>
           </div>
-          <div className="goal basis-1/3">
+          <div className="goal basis-1/3 mb-4">
             <img src="/objective.png" alt="objective" />
             <div className="goaltitle">Objective</div>
             <div className="goaltext">

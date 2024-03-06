27f1a8fdf7649a2a56dd4a70adca462200ddd17b
Dipesh Maharjan <dipesh.maharjan@peacenepal.com.np>
2024-03-06 09:46:06 +0545

    added transition effect

--- app/components/common/SideMenu.tsx
+++ app/components/common/SideMenu.tsx
@@ -8,29 +8,29 @@ import { FaMoneyCheckDollar } from "react-icons/fa6";
 import { HiOutlinePhone } from "react-icons/hi";
 const SideMenu = () => {
   return (
-    <div className="fixed top-56 z-50 hidden h-[510px] w-[65px] justify-evenly rounded-r-md bg-custom-red md:flex">
+    <div className="fixed top-28 z-50 hidden h-[510px] w-[65px] justify-evenly rounded-r-md bg-custom-red md:flex">
       <ul className="flex  flex-col justify-evenly text-sm text-white ">
-        <li className="flex cursor-pointer flex-col items-center justify-center space-y-2 text-xs hover:text-custom-light-red">
+        <li className="hoverTextRedEffect flex cursor-pointer flex-col items-center justify-center space-y-2 text-xs">
           <GrAtm className="text-lg" />
           <span>ATM</span>
         </li>
-        <li className="flex cursor-pointer flex-col  items-center justify-center space-y-1 text-xs hover:text-custom-light-red">
+        <li className="hoverTextRedEffect flex cursor-pointer  flex-col items-center justify-center space-y-1 text-xs">
           <IoIosGitBranch className="text-lg" />
           <span>Branch</span>
         </li>
-        <li className="flex cursor-pointer flex-col  items-center justify-center space-y-1 text-xs hover:text-custom-light-red">
+        <li className="hoverTextRedEffect flex cursor-pointer  flex-col items-center justify-center space-y-1 text-xs">
           <IoGiftOutline className="text-lg" />
           <span>Offers</span>
         </li>
-        <li className="flex cursor-pointer flex-col  items-center justify-center space-y-1  text-xs hover:text-custom-light-red">
+        <li className="hoverTextRedEffect flex cursor-pointer  flex-col items-center justify-center  space-y-1 text-xs">
           <AiOutlinePercentage className="text-lg" />
           <span className="text-center">Interest Rate</span>
         </li>
-        <li className="flex cursor-pointer flex-col  items-center justify-center space-y-1 text-xs hover:text-custom-light-red">
+        <li className="hoverTextRedEffect flex cursor-pointer  flex-col items-center justify-center space-y-1 text-xs">
           <FaMoneyCheckDollar className="text-lg" />
           <span> Forex </span>
         </li>
-        <li className="flex cursor-pointer flex-col  items-center justify-center space-y-1 text-xs hover:text-custom-light-red">
+        <li className="hoverTextRedEffect flex cursor-pointer  flex-col items-center justify-center space-y-1 text-xs">
           <HiOutlinePhone className="text-lg" />
           <span>Contact</span>
         </li>

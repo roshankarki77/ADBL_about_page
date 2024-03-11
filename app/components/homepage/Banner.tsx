32a2c189ccd822b904d47651c0f6eb8acb8428c5
Roshan <roshan77karki77@gmail.com>
2024-03-11 10:30:13 +0545

    mobbnk

--- app/components/homepage/Banner.tsx
+++ app/components/homepage/Banner.tsx
@@ -9,7 +9,7 @@ const Banner = () => {
       <div className="w-[95%] sm:w-[85%] mx-auto 2xl:max-w-[1320px]">
         <div className="flex flex-wrap">
           <div className="lg:flex-1 w-full"  >
-            <img src={"/images/mobile-app.png"} className="w-full hidden object-cover" alt="mobile app" />
+            <img src={"/images/mobile-app.png"} className="w-full hidden object-cover lg:block" alt="mobile app" />
           </div>
           <div className=" flex lg:flex-1 flex-col lg:pl-8 lg:pt-0 space-y-4  justify-center">
             <h2 className="text-2xl font-semibold">

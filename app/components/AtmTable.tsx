1d2a255c958670abca15baacf892625b5b426d0b
Roshan <roshan77karki77@gmail.com>
2024-03-20 17:46:47 +0545

    icon added

--- app/components/AtmTable.tsx
+++ app/components/AtmTable.tsx
@@ -20,15 +20,15 @@ function AtmTable({
   const [pagination, setPagination] = useState<PaginationState>({
     pageIndex: 0,
     pageSize: 10,
-  })
+  });
   const [filtering, setFiltering] = useState("");
 
-  console.log("data",data)
+  console.log("data", data);
   const columns = [
     {
       header: "SN",
       accessorKey: "",
-      cell: (row:any) => {
+      cell: (row: any) => {
         return <div>{parseInt(row.row.id) + 1}</div>;
       },
       disableSortBy: true,
@@ -51,13 +51,26 @@ function AtmTable({
     {
       header: "Contact Details",
       accessorKey: "address",
-      Cell: ({ row }: { row: any }) => {
+      cell: ({ row }: { row: any }) => {
         return (
           <div className="contact-details">
             <ul>
               {row.original.address == null ? null : (
-                <li>
-                  <i className="bi bi-geo-alt"></i> {row.original.address}
+                <li className="align-center flex">
+                  <span className="mr-2 flex pt-1">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="16"
+                      height="16"
+                      fill="currentColor"
+                      className="bi bi-geo-alt"
+                      viewBox="0 0 16 16"
+                    >
+                      <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A32 32 0 0 1 8 14.58a32 32 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10" />
+                      <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4m0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                    </svg>
+                  </span>
+                  {row.original.address}
                 </li>
               )}
             </ul>
@@ -82,32 +95,31 @@ function AtmTable({
   });
   // console.log(table.getRowModel().rows)
   //   console.log(table.getRowModel().flatRows);
-  console.log("globalFilter",table
-  )
+  console.log("globalFilter", table);
 
   const handleSetFirstPage = () => {
     table.firstPage();
-  } 
+  };
   const handleSetNextPage = () => {
     table.nextPage();
-  } 
-  const handleCanSetNextPage = () => table.getCanNextPage()
+  };
+  const handleCanSetNextPage = () => table.getCanNextPage();
 
   const handleSetPreviousPage = () => {
     table.previousPage();
-  } 
-  const handleCanSetPreviousPage = () =>  table.getCanPreviousPage()
+  };
+  const handleCanSetPreviousPage = () => table.getCanPreviousPage();
 
-  const handleGetCurrentPage = () => table.getState().pagination.pageIndex + 1
+  const handleGetCurrentPage = () => table.getState().pagination.pageIndex + 1;
 
-  const handleSetCurrentPage = (e:any) => {
-    const page = e ? e - 1 : 0
-    table.setPageIndex(page)
-  }
+  const handleSetCurrentPage = (e: any) => {
+    const page = e ? e - 1 : 0;
+    table.setPageIndex(page);
+  };
 
-  const handleGetTotalPages = () => table.getPageCount()
+  const handleGetTotalPages = () => table.getPageCount();
   const [detectSelectChange, setDetectSelectChange] = useState(false);
-  const [detectFilteringChange,setDetectFilteringChange] = useState(false);
+  const [detectFilteringChange, setDetectFilteringChange] = useState(false);
   return (
     <div className="overflow-x-auto">
       <div className="mb-4 flex flex-col justify-between sm:flex-row">
@@ -115,19 +127,19 @@ function AtmTable({
           <label>
             Show
             <div className="mx-2 inline-block border-2 border-custom-green">
-            <select
-          value={table.getState().pagination.pageSize}
-          onChange={e => {
-            table.setPageSize(Number(e.target.value));
-            setDetectSelectChange(!detectSelectChange);
-          }}
-        >
-          {[10, 20, 30].map(pageSize => (
-            <option key={pageSize} value={pageSize}>
-               {pageSize}
-            </option>
-          ))}
-        </select>
+              <select
+                value={table.getState().pagination.pageSize}
+                onChange={(e) => {
+                  table.setPageSize(Number(e.target.value));
+                  setDetectSelectChange(!detectSelectChange);
+                }}
+              >
+                {[10, 20, 30].map((pageSize) => (
+                  <option key={pageSize} value={pageSize}>
+                    {pageSize}
+                  </option>
+                ))}
+              </select>
             </div>
             entries
           </label>
@@ -137,7 +149,10 @@ function AtmTable({
           <input
             type="text"
             value={filtering}
-            onChange={(e) => {setFiltering(e.target.value);setDetectFilteringChange(!detectFilteringChange)}}
+            onChange={(e) => {
+              setFiltering(e.target.value);
+              setDetectFilteringChange(!detectFilteringChange);
+            }}
             className="w-36 border-2 border-custom-green"
           />
         </div>
@@ -147,7 +162,7 @@ function AtmTable({
           {table.getHeaderGroups().map((headerGroup) => (
             <tr key={headerGroup.id} className="bg-custom-green ">
               {headerGroup.headers.map((header) => {
-                console.log("headers",header)
+                console.log("headers", header);
                 return (
                   <th
                     key={header.id}
@@ -181,21 +196,20 @@ function AtmTable({
           })}
         </tbody>
       </table>
-        <Pagination2
-          table={table}
-          data={data}
-          detectSelectChange={detectSelectChange}
-          detectFilteringChange={detectFilteringChange}
-          handleSetFirstPage={handleSetFirstPage}
-          handleSetNextPage={handleSetNextPage}
-          handleCanSetNextPage={handleCanSetNextPage}
-          handleSetPreviousPage={handleSetPreviousPage}
-          handleCanSetPreviousPage={handleCanSetPreviousPage}
-          handleGetCurrentPage={handleGetCurrentPage}
-          handleSetCurrentPage={handleSetCurrentPage}
-          handleGetTotalPages={handleGetTotalPages}
-        />
-      
+      <Pagination2
+        table={table}
+        data={data}
+        detectSelectChange={detectSelectChange}
+        detectFilteringChange={detectFilteringChange}
+        handleSetFirstPage={handleSetFirstPage}
+        handleSetNextPage={handleSetNextPage}
+        handleCanSetNextPage={handleCanSetNextPage}
+        handleSetPreviousPage={handleSetPreviousPage}
+        handleCanSetPreviousPage={handleCanSetPreviousPage}
+        handleGetCurrentPage={handleGetCurrentPage}
+        handleSetCurrentPage={handleSetCurrentPage}
+        handleGetTotalPages={handleGetTotalPages}
+      />
     </div>
   );
 }
